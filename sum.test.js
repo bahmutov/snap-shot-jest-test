0a6428fa9f93d9a529a8940796b4df7913706974
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-02-04 23:43:28 -0500

    passing tests with snap-shot

--- sum.test.js
+++ sum.test.js
@@ -1,6 +1,12 @@
-const sum = require('./sum');
+const sum = require('./sum')
+const snapshot = require('snap-shot')
 
 test('adds 1 + 2 to equal 3', () => {
   const result = sum(1, 2)
-  expect(result).toMatchSnapshot();
+  expect(result).toMatchSnapshot()
+})
+
+test('2 + 5 should match snap-shot', () => {
+  const result = sum(2, 5)
+  snapshot(result)
 })
