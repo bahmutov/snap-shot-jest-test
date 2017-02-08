2ab86a53d467886d4a2c364c6d5488f807117729
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-02-07 22:36:23 -0500

    show snapshot testing for React, close #1

--- Link.test.js
+++ Link.test.js
@@ -3,24 +3,26 @@ import Link from './Link';
 import renderer from 'react-test-renderer';
 import snapshot from 'snap-shot'
 
-test.skip('Link changes the class when hovered', () => {
+// removes Function values (and other non-serializable stuff)
+const strip = o => JSON.parse(JSON.stringify(o))
+
+test('Link changes the class when hovered', () => {
   // snapshot does not know how to handle JSX
   const component = renderer.create(
     <Link page="http://www.facebook.com">Facebook</Link>
   );
   let tree = component.toJSON();
-  snapshot(tree)
-  // expect(tree).toMatchSnapshot();
+  snapshot(strip(tree))
 
-  // // manually trigger the callback
-  // tree.props.onMouseEnter();
-  // // re-rendering
-  // tree = component.toJSON();
-  // expect(tree).toMatchSnapshot();
+  // manually trigger the callback
+  tree.props.onMouseEnter();
+  // re-rendering
+  tree = component.toJSON();
+  snapshot(strip(tree))
 
-  // // manually trigger the callback
-  // tree.props.onMouseLeave();
-  // // re-rendering
-  // tree = component.toJSON();
-  // expect(tree).toMatchSnapshot();
+  // manually trigger the callback
+  tree.props.onMouseLeave();
+  // re-rendering
+  tree = component.toJSON();
+  snapshot(strip(tree))
 });
